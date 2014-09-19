38907949abb8f3a78757f13f6f2dafdbdd229b53
Sumit Bindal <sumit.bindal@stonybrook.edu>
2014-09-18 23:06:12 -0400

    q1-->dfs

--- util.py
+++ util.py
@@ -32,6 +32,10 @@ class Stack:
     "Returns true if the stack is empty"
     return len(self.list) == 0
 
+  def getList(self):
+    "Returns the complete stack"
+    return self.list
+
 class Queue:
   "A container with a first-in-first-out (FIFO) queuing policy."
   def __init__(self):
@@ -51,6 +55,10 @@ class Queue:
   def isEmpty(self):
     "Returns true if the queue is empty"
     return len(self.list) == 0
+
+  def getList(self):
+    "Returns the complete queue"
+    return self.list
   
 class PriorityQueue:
   """
